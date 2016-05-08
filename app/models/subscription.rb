246fea64280be0020c76e1b730e42dfe5f999475
Eric Chen <eric87chen@gmail.com>
2016-05-07 23:12:36 -0400

    Added Validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,6 @@ class Subscription < ActiveRecord::Base
   EARLIEST_HOUR = 5
   LASTEST_HOUR = 23
   RANDOM_HOUR = 99
+
+  validates :user_id, :time_zone, :phone, :send_hour, presence: true
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :time_zone }
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :send_hour }
+  end
 end
