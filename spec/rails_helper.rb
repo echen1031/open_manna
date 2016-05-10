9887234f8407e7f35b6d393a42fcd77bf13f0e89
Eric Chen <eric87chen@gmail.com>
2016-05-10 12:23:49 -0400

    stubbed nexmo http request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,11 +7,15 @@ require 'webmock/rspec'
 require 'support/factory_girl'
 require 'support/warden'
 require 'support/shoulda_matchers'
+require 'support/rspec_sidekiq'
 
 WebMock.disable_net_connect!(allow_locahost: true)
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:post, "https://rest.nexmo.com/sms/json").with(:body => {"api_key"=>ENV['NEXMO_API_KEY'], "api_secret"=>ENV['NEXMO_API_SECRET'], "from"=>"Open Manna", "text"=>"This is a test verse","to"=>"15555555555"}, :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'ruby-nexmo/4.1.0/2.2.1'}).to_return(:status => 200, :body => "", :headers => {})
+  end
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
