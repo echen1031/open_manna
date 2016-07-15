949ecaa4bb4c3ca4574e046537328ad93b6d5a1d
Eric Chen <eric87chen@gmail.com>
2016-07-15 17:15:01 -0400

    updated factory

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -4,6 +4,12 @@ FactoryGirl.define do
   factory :subscription do
     sequence(:user_id) { |n| n }
     send_day_1 true
+    send_day_2 false
+    send_day_3 false
+    send_day_4 false
+    send_day_5 false
+    send_day_6 false
+    send_day_7 false
     send_hour 7
     time_zone { Faker::Address.time_zone }
     phone "15555555555"
