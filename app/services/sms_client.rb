bb91e6fda2e9237cf365c06bda6b41cdd421a832
Eric Chen <eric87chen@gmail.com>
2016-07-18 15:05:54 -0400

    next client testing

--- app/services/sms_client.rb
+++ app/services/sms_client.rb
@@ -4,9 +4,10 @@ class SMSClient
   def initialize
     @client = Nexmo::Client.new(key: ENV['NEXMO_API_KEY'],
                                     secret: ENV['NEXMO_API_SECRET'])
+    @from = ENV['NEXMO_PHONE']
   end
 
-  def send_message(from:, to:, text:)
-    @client.send_message(from: from, to: to, text: text)
+  def send_message(to:, text:)
+    @client.send_message(from: @from, to: to, text: text)
   end
 end
