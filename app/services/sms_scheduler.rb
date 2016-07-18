8fc617aace83d2ee3af9d2e86da6b7e7099f52ef
Eric Chen <eric87chen@gmail.com>
2016-07-18 15:06:26 -0400

    updated sms scheduler with tests

--- app/services/sms_scheduler.rb
+++ app/services/sms_scheduler.rb
@@ -1,9 +1,9 @@
 class SMS_Scheduler
-  def self.set_daily_sms_job(subscription)
+  def self.set_daily_sms_job(subscription, verse)
     if subscription.sms_to_send_today?
       Time.zone = subscription.time_zone
       user_preferred_time_in_utc = Time.now.change(hour: subscription.send_hour).utc
-      DailySMSWorker.perform_at(user_preferred_time_in_utc, subscription.id)
+      DailySMSWorker.perform_at(user_preferred_time_in_utc, subscription.id, verse.id)
     end
   end
 end
--- spec/services/sms_scheduler_spec.rb
+++ spec/services/sms_scheduler_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe SMS_Scheduler do
   it "schedules text sending according to the user's preferences" do
     sub = create(:subscription, send_day_1: true, send_hour: 7, time_zone: "Eastern Time (US & Canada)")
+    verse = create(:verse)
     Timecop.travel("2016-07-11")
-    SMS_Scheduler.set_daily_sms_job(sub)
+    SMS_Scheduler.set_daily_sms_job(sub, verse)
     expect(DailySMSWorker.jobs.size).to eq 1
     Timecop.return
   end
