acdecbbd60f3e34f1d1f3f65e6791583c6bde415
Eric Chen <eric87chen@gmail.com>
2016-07-18 15:07:32 -0400

    defined what to performed on sms worker

--- app/workers/daily_sms_worker.rb
+++ app/workers/daily_sms_worker.rb
@@ -1,6 +1,10 @@
 class DailySMSWorker
   include Sidekiq::Worker
 
-  def perform(subscription)
+  def perform(subscription_id, verse_id)
+    raise 'invalid request' if subscription_id.nil? or verse_id.nil?
+    phone = Subscription.find(subscription_id).phone
+    bible_verse = VerseDecorator.decorate(Verse.find(verse_id))
+    SMSClient.new.send_message(to: phone, text: bible_verse.text_message)
   end
 end
