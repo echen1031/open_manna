55b5ce7ddc0de3db5ca12e1c35ca46f158ff7ac2
Eric Chen <eric87chen@gmail.com>
2016-07-25 11:34:59 -0400

    readability refactoring

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,21 +3,16 @@ class Subscription < ActiveRecord::Base
   EARLIEST_HOUR = 5
   LASTEST_HOUR = 23
   RANDOM_HOUR = 99
-  DAYS_CONVERTER = {
-    1 => "send_monday",
-    2 => "send_tuesday",
-    3 => "send_wednesday",
-    4 => "send_thursday",
-    5 => "send_friday",
-    6 => "send_saturday",
-    0 => "send_sunday"
-  }
-
 
   validates :user_id, :time_zone, :phone, :send_hour, presence: true
 
   def no_sms_for_today
-    current_day = Time.now.wday
-    self.send(DAYS_CONVERTER[current_day]) == false
+    self.send(current_day_in_words) == false
+  end
+
+  def current_day_in_words
+    Time.zone = self.time_zone
+    current_day = Time.now.strftime("%A").downcase
+    "send_#{current_day}"
   end
 end
