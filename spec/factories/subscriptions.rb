20aae38705bd10b4640183be81e4e2551b96a5bf
Eric Chen <eric87chen@gmail.com>
2016-07-30 13:29:28 -0400

    Updated feature test

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -3,6 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :subscription do
     sequence(:user_id) { |n| n }
+    name { Faker::Name.name }
     send_hour 7
     time_zone { Faker::Address.time_zone }
     phone "15555555555"
--- spec/features/user_manages_subscription_spec.rb
+++ spec/features/user_manages_subscription_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 feature "User manages his or her subscription" do
+  let(:user) { create(:user) }
   before(:each) do
-    user = create(:user)
     login_as user
   end
 
   scenario "creates subscription successfully" do
-    user = User.first
     visit new_user_subscription_path(user.id)
+    fill_in 'Name', with: 'Morning Revival'
     fill_in 'Phone', with: '555-555-5555'
     select('Eastern', :from => 'Time zone')
     check 'subscription_send_monday'
@@ -19,5 +19,14 @@ feature "User manages his or her subscription" do
     subscription = Subscription.first
     expect(subscription.send_monday).to eq true
     expect(subscription.user_id).to eq user.id
+    expect(subscription.name).to eq 'Morning Revival'
+  end
+
+  scenario "sees all subscriptions that belongs to him" do
+    subscriptions = create_list(:subscription, 2)
+    visit user_subscriptions_path(user.id)
+    subscriptions.each do |s|
+      expect(page).to have_content("#{s.name}")
+    end
   end
 end
