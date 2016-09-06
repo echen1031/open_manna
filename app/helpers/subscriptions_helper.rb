065b3b8c51a39b0f0805c73d15cc5c475938a1a5
Eric Chen <eric87chen@gmail.com>
2016-09-05 22:45:51 -0400

    Modify earliest and latest hour, fix typo

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,7 +1,7 @@
 module SubscriptionsHelper
 
   def select_hours
-    [['Random (8 am ~ 10 pm)', Subscription::RANDOM_HOUR]] + 
+    [['Random (8 am ~ 8 pm)', Subscription::RANDOM_HOUR]] + 
       (Subscription::EARLIEST_HOUR..Subscription::LASTEST_HOUR).map {|h| [Time.parse("#{h}:00").strftime("%l %P"), h ] }
   end 
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,7 +1,7 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
-  EARLIEST_HOUR = 5
-  LASTEST_HOUR = 23
+  EARLIEST_HOUR = 6
+  LASTEST_HOUR = 21
   RANDOM_HOUR = 99
 
   validates :user_id, :time_zone, :phone_number, :send_hour, presence: true
