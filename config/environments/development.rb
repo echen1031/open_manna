7313403336a740d5bb0df0f611a0c016258ac4c6
Eric Chen <eric87chen@gmail.com>
2016-09-13 11:01:21 -0400

    Updated config development and production mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = { 
+    :address => "smtp.gmail.com", 
+    :port => 587, 
+    :authentication => :plain, 
+    :domain => 'gmail.com', 
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
+  }
 end
