cc6e79de1d420e6d495ada04162e68c8f70c9cab
Eric Chen <eric87chen@gmail.com>
2016-09-13 10:19:41 -0400

    authentication to plain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Mailer
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'openmanna.com' }
 
   config.action_mailer.delivery_method = :smtp
@@ -90,7 +89,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: :login,
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
