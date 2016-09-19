9da05f82812066a9d15aad1001b0ffbba357c829
Eric Chen <eric87chen@gmail.com>
2016-09-19 13:17:04 -0400

    switched name to SMSClientLogger

--- app/workers/daily_sms_worker.rb
+++ app/workers/daily_sms_worker.rb
@@ -7,7 +7,7 @@ class DailySmsWorker
     bible_verse = VerseDecorator.decorate(Verse.find(verse_id))
     response = SMSClient.new.send_message(to: phone_number, text: bible_verse.text_message)
     nexmo_response = NexmoResponseManager.new(response)
-    SmsClientLogger.create(status_code: nexmo_response.status_code, status_text: nexmo_response.status_text, message_id: nexmo_response.message_id, to: nexmo_response.to)
+    SMSClientLogger.create(status_code: nexmo_response.status_code, status_text: nexmo_response.status_text, message_id: nexmo_response.message_id, to: nexmo_response.to)
 
     if nexmo_response.successful
       SubscriptionVerse.create(subscription_id: subscription_id, verse_id: verse_id)
