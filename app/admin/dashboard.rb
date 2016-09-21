e1515721043603ea78afc244dfd26a240b3b7412
Eric Chen <eric87chen@gmail.com>
2016-09-21 10:02:20 -0400

    Fixed admin dashboard views

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,13 +9,17 @@ ActiveAdmin.register_page "Dashboard" do
           table_for SubscriptionVerse.order("created_at desc").limit(10) do
             column :id
             column :subscription_id
-            column "User", :subscription_id do |sub|
-              Subscription.find(sub).user.email
+            column "User", :subscription_id do |sub_verse|
+              Subscription.find(sub_verse.subscription_id).user.email
             end
-            column :verse_id do |verse|
-              Verse.find(verse).reference
+            column :verse_id do |sub_verse|
+              Verse.find(sub_verse.verse_id).reference
             end
-            column :created_at
+            column :created_at do |sub_verse|
+              Time.use_zone("Eastern Time (US & Canada)") do
+                sub_verse.created_at.strftime("%m/%d/%Y at %l:%M %p (est)")
+              end
+            end
           end
         end
       end
--- app/admin/subscription_verse.rb
+++ app/admin/subscription_verse.rb
@@ -2,12 +2,16 @@ ActiveAdmin.register SubscriptionVerse do
   index do
     column :id
     column :subscription_id
-    column "User", :subscription_id do |sub|
-      Subscription.find(sub).user.email
+    column "User", :subscription_id do |sub_verse|
+      Subscription.find(sub_verse.subscription_id).user.email
     end
-    column :verse_id do |verse|
-      Verse.find(verse).reference
+    column :verse_id do |sub_verse|
+      Verse.find(sub_verse.verse_id).reference
     end
-    column :created_at
+    column :created_at do |sub_verse|
+      Time.use_zone("Eastern Time (US & Canada)") do
+        sub_verse.created_at.strftime("%m/%d/%Y at %l:%M %p (est)")
+      end
+    end
   end
 end
