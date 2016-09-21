9d5c2d6300a23c78e134720fa36a8e47eb3a501d
Eric Chen <eric87chen@gmail.com>
2016-09-21 08:45:31 -0400

    Authenticate admin for sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
   resources :users
+  resources :verifications, only: [:edit, :update]
   resources :subscriptions do
     member do
       get 'pause', as: 'pause'
@@ -13,9 +15,11 @@ Rails.application.routes.draw do
   end
 
   get 'start', as: 'start_verification', controller: 'verifications'
-  resources :verifications, only: [:edit, :update]
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
 
   root to: 'welcome#index'
-  mount Sidekiq::Web, at: '/sidekiq'
   match '*path', via: :all, to: redirect('/404')
 end
