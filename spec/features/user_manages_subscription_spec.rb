a046e3d9046bfe20af979fa57b76f260dd24af86
Eric Chen <eric87chen@gmail.com>
2016-09-22 01:08:49 -0400

    Took out a feature spec into it's own test

--- spec/features/user_manages_subscription_spec.rb
+++ spec/features/user_manages_subscription_spec.rb
@@ -107,31 +107,4 @@ feature "User manages his or her subscription" do
     click_link "Pause Subscription"
     expect(Subscription.first.active).to eq false
   end
-
-  scenario "activates his subscription successfully" do
-    sub = create(:inactive_subscription, user_id: user.id)
-    send_result = double("send_result")
-    request_id = "12345"
-    code = "5555"
-    allow(VerificationRequestSender).to receive(:new).with(sub.phone_number).and_return(send_result)
-    allow(send_result).to receive(:send_successful_request?).and_return true
-    allow(send_result).to receive(:request_id).and_return request_id
-
-    visit subscriptions_path
-    click_link "Verify"
-
-    expect(send_result).to have_received(:send_successful_request?)
-    expect(send_result).to have_received(:request_id)
-    expect(page).to have_content("Verification Code")
-
-    check_result = double("check_result")
-    allow(VerificationRequestChecker).to receive(:new).with(request_id, code).and_return(check_result)
-    allow(check_result).to receive(:phone_number_confirmed?).and_return true
-    fill_in "Code", with: code
-    click_button "Verify"
-
-    sub.reload
-    expect(check_result).to have_received(:phone_number_confirmed?)
-    expect(sub.active).to eq true
-  end
 end
