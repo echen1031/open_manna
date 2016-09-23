ee37a11a7c7bf95fe6e45d887d135fae27f3faef
Eric Chen <eric87chen@gmail.com>
2016-09-22 22:12:25 -0400

    Refactor admin page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,15 +9,14 @@ ActiveAdmin.register_page "Dashboard" do
           table_for SubscriptionVerse.order("created_at desc").limit(10) do
             column :id
             column :subscription_id
-            column "User", :subscription_id do |sub_verse|
-              Subscription.find(sub_verse.subscription_id).user.email
-            end
+            column :user_id
             column :verse_id do |sub_verse|
               Verse.find(sub_verse.verse_id).reference
             end
             column :created_at do |sub_verse|
-              Time.use_zone("Eastern Time (US & Canada)") do
-                sub_verse.created_at.strftime("%m/%d/%Y at %l:%M %p (est)")
+              sub = sub_verse.subscription
+              Time.use_zone(sub.time_zone) do
+                sub_verse.created_at.strftime("%m/%d/%Y at %l:%M %p")
               end
             end
           end
