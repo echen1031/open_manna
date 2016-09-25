d8658aff369fde18ec229b4b53b4f88d6e20f952
Eric Chen <eric87chen@gmail.com>
2016-09-24 22:54:05 -0400

    welcome index page styling

---
+++ app/services/open_manna_constants.rb
@@ -0,0 +1,6 @@
+class OpenMannaConstants
+  URL = "https://www.openmanna.com"
+  NAME = "OpenManna"
+  PHOTO = "https://s3.amazonaws.com/openmanna/FullSizeRender.jpg"
+  DISCRIPTION = "Receive daily, nourishing verses straight to your phone."
+end
