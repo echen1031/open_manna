d2ed94082fd905d4df60ae31cf1bdbb4cf26084f
Eric Chen <eric87chen@gmail.com>
2016-11-07 11:09:06 -0500

    Removed logger and not storing subscription_verses

--- spec/workers/daily_sms_worker_spec.rb
+++ spec/workers/daily_sms_worker_spec.rb
@@ -19,13 +19,7 @@ describe DailySMSWorker do
       expect(SMSClient).to receive(:new).and_return(sms_client)
       expect(sms_client).to receive(:send_message).with(to: phone_number, text: verse_text).and_return(response)
       expect(NexmoResponseManager).to receive(:new).with(response).and_return(nexmo_response)
-      expect(nexmo_response).to receive(:status_code).and_return(status_code)
-      expect(nexmo_response).to receive(:status_text).and_return(status_text)
-      expect(nexmo_response).to receive(:message_id).and_return(msg_id)
-      expect(nexmo_response).to receive(:to).and_return(to)
-      expect(SMSClientLogger).to receive(:create).with(status_code: status_code, status_text: status_text, message_id: msg_id, to: to)
-      expect(nexmo_response).to receive(:successful).and_return(true)
-      expect(SubscriptionVerse).to receive(:create).with(subscription_id: subscription.id, verse_id: verse.id, user_id: user.id)
+      expect(nexmo_response).to receive(:throttled).and_return(false)
 
       DailySMSWorker.new.perform(subscription.id, verse.id)
     end
