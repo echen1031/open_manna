3ad852e60f259043c1d4a61135bf2c0d64fdf4a0
Eric Chen <eric87chen@gmail.com>
2017-12-14 17:22:48 -0600

    Upgrade rails version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,18 @@ Rails.application.configure do
   #Mailer
   config.action_mailer.default_url_options = { host: 'openmanna.com' }
 
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :address => "email-smtp.us-east-1.amazonaws.com",
+    :port => 587,
+    :user_name => ENV["SES_SMTP_USERNAME"],
+    :password => ENV["SES_SMTP_PASSWORD"], 
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
 
 end
