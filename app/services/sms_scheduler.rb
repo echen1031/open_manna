84c3c2311b895da27c93fba6e7143e454d4a8b62
Eric Chen <eric87chen@gmail.com>
2018-02-12 00:24:24 -0600

    added stored_verse_ids to subscription, modify SMS scheduler to store verse to subscription.

--- app/services/sms_scheduler.rb
+++ app/services/sms_scheduler.rb
@@ -4,7 +4,8 @@ class SMSScheduler
       next if sub.no_sms_for_today
       @time_to_send = (sub.send_hour == 99 ) ? (8..20).to_a.sample : sub.send_hour
       @sub = sub
-      @random_verse = Verse::random
+      @random_verse = sub.retrieve_random_verse
+      sub.store_verse(@random_verse)
 
       Time.use_zone(sub.time_zone) do
         user_preferred_time_in_utc = Time.zone.now.change(hour: @time_to_send).utc
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -22,4 +22,36 @@ RSpec.describe Subscription, type: :model do
       expect(Subscription.active.size).to eq 1
     end
   end
+
+  describe "#retrieve_random_verse" do
+    it "does not send same verse twice without going through whole cycle" do
+      create_list(:verse, 2)
+      sub = create(:subscription, stored_verse_ids: [2])
+      verse = sub.retrieve_random_verse
+      expect(verse.id).to eq 1
+    end
+  end
+
+  describe "#store_verse" do
+    it "stores the verse id into subscription" do
+      create_list(:verse, 2)
+      sub = create(:subscription)
+      verse = Verse.first
+      sub.store_verse(verse)
+      sub.reload
+      expect(sub.stored_verse_ids).to eq [verse.id]
+    end
+
+    it "resets the array to empty when reached all verse ids" do
+      verse_1 = create(:verse)
+      verse_2 = create(:verse)
+      verse_3 = create(:verse)
+      verse_4 = create(:verse)
+      sub = create(:subscription, stored_verse_ids: [verse_1.id, verse_3.id, verse_4.id])
+
+      expect(sub.retrieve_random_verse).to eq verse_2
+      sub.store_verse(sub.retrieve_random_verse)
+      expect(sub.stored_verse_ids).to eq []
+    end
+  end
 end
