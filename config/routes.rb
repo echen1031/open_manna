2c9081863340e6a6618f06960da699b8ef2baee2
Eric Chen <eric87chen@gmail.com>
2018-03-03 17:10:32 -0600

    Remove donations code for now

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   end
 
   get 'start', as: 'start_verification', controller: 'verifications'
-  get 'donate', as: 'donate', controller: 'donations'
 
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web, at: '/sidekiq'
