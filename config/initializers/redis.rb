90ddf588b449c53bee1e9eee8732289a1f3c42ba
Eric Chen <eric@cratebind.com>
2021-12-31 16:21:01 -0600

    remove namespace

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,10 @@
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: "#{ENV['REDIS_URL']}", namespace: 'openmanna' }
+    config.redis = { url: "#{ENV['REDIS_URL']}"}
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: "#{ENV['REDIS_URL']}", namespace: 'openmanna' }
+    config.redis = { url: "#{ENV['REDIS_URL']}"}
   end
 end
 
