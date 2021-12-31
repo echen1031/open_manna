afc85b6fccf66a775216e8cf813153694ed30c41
Eric Chen <eric@cratebind.com>
2021-12-31 16:18:03 -0600

    update redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,27 @@
-if ENV["REDIS_URL"]
-  uri = URI.parse(ENV["REDIS_URL"])
-  REDIS = Redis.new(:url => uri)
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "#{ENV['REDIS_URL']}", namespace: 'openmanna' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "#{ENV['REDIS_URL']}", namespace: 'openmanna' }
+  end
+end
+
+if Rails.env.test? || Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/1", namespace: 'staging' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/1", namespace: 'staging' }
+  end
+elsif Rails.env.staging?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "#{ENV['REDISTOGO_URL']}", namespace: 'staging' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "#{ENV['REDISTOGO_URL']}", namespace: 'staging' }
+  end
 end
