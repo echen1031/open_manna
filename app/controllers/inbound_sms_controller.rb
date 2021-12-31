8fcbb107fe3acbd24ca8c4d12402548480a8bbdc
Eric Chen <eric@cratebind.com>
2021-12-30 22:51:05 -0600

    Allow user to stop or resume subscription via sms reply

--- app/controllers/inbound_sms_controller.rb
+++ app/controllers/inbound_sms_controller.rb
@@ -1,41 +1,30 @@
 class InboundSmsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, only: [:message_status_webhook, :inbound_message_webhook]
 
-  def create
-    # Creates a new SMS record in
-    # the database
-    sms = Sms.create(
-      to: params[:to],
-      from: params[:msisdn],
-      text: params[:text],
-      message_id: params[:messageId],
-      is_inbound: true
-    )
-
-    # Send a reply
-    reply sms
-
-    # Return an empty HTTP 200 status regardless
+  def message_status_webhook
     head :ok
   end
 
-  private
-
-  # Initializes the Nexmo API client
-  def nexmo
-    # We do not pass in any API key or secret as
-    # we're using environment variables `NEXMO_API_KEY`
-    # and `NEXMO_API_SECRET`
-    client = Nexmo::Client.new
-  end
-
-  # Uses the Nexmo API to send a quick reply to the SMS received
-  # for simplicity we're not storing this one in the database
-  def reply sms
-    nexmo.send_message(
-      from: sms.to,
-      to: sms.from,
-      text: sms.text.reverse
-    )
+  def inbound_message_webhook
+    if params['keyword'] == 'STOP'
+      sub = Subscription.find_by(phone_number: '+' + params["msisdn"])
+      if sub.present? && sub.active == true
+        sub.update_columns(active: false)
+        SMSClient.new.send_message(
+          to: sub.phone_number,
+          text: 'Successfully stopped subscription. Reply START to resume'
+        )
+      end
+    elsif params['keyword'] == 'START'
+      sub = Subscription.find_by(phone_number: '+' + params["msisdn"])
+      if sub.present? && sub.active == false && sub.verified == true
+        sub.update_columns(active: true)
+        SMSClient.new.send_message(
+          to: sub.phone_number,
+          text: 'Successfully resumed subscription. Reply STOP to stop'
+        )
+      end
+    end
+    head :ok
   end
 end
