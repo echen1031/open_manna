035e663b5f62ffa447f6bbfef8003ae950c0a10b
echen1031 <eric87chen@gmail.com>
2022-01-04 23:34:25 -0600

    add sms throttling handler to resend sms (#56)
    Co-authored-by: Eric Chen <eric@cratebind.com>

--- app/workers/daily_sms_worker.rb
+++ app/workers/daily_sms_worker.rb
@@ -7,16 +7,14 @@ class DailySMSWorker
     sub = Subscription.find(subscription_id)
     phone_number = sub.phone_number
     bible_verse = VerseDecorator.decorate(Verse.find(verse_id))
-    response = SMSClient.new.send_message(to: phone_number, text: bible_verse.text_message)
-    vonage_response = VonageResponseManager.new(response)
-    if vonage_response.throttled
-      resend_message(subscription_id, verse_id) 
+    begin
+      SMSClient.new.send_message(to: phone_number, text: bible_verse.text_message)
+    rescue => e
+      if e.message.include?('Throughput Rate Exceeded')
+        random_interval = rand(2..60).seconds
+        DailySMSWorker.perform_in(random_interval, sub.id, verse_id) #rerun worker
+      end
     end
   end
-end
-
-def resend_message(sub_id, verse_id)
-  random_interval = rand(2..60).seconds
-  DailySMSWorker.perform_in(random_interval, sub_id, verse_id)
 end
 
